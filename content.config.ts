2052d5302ca3067be05a0c8bb6f0ccd5313a57bd
o0olele <lr18516312@163.com>
2026-03-24 01:02:46 +0800

    [blog] envs

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,25 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    landing: defineCollection({
       type: 'page',
-      source: '**',
+      source: 'index.md'
     }),
-  },
+    docs: defineCollection({
+      type: 'page',
+      source: {
+        include: '**',
+        exclude: ['index.md']
+      },
+      schema: z.object({
+        links: z.array(z.object({
+          label: z.string(),
+          icon: z.string(),
+          to: z.string(),
+          target: z.string().optional()
+        })).optional()
+      })
+    })
+  }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
     '@nuxt/content',
+    'nuxt-og-image',
   ],
-  devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
+
+  devtools: {
+    enabled: false
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          searchDepth: 1
+        }
+      }
+    }
+  },
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true,
+      autoSubfolderIndex: false
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  icon: {
+    provider: 'iconify'
+  },
+
+  ui: {
+    fonts: false
+  },
 })
