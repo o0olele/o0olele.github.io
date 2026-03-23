718a643e75057fa941b560afc46454cfded618d1
o0olele <lr18516312@163.com>
2026-03-24 01:02:19 +0800

    [blog] templates

---
+++ app/app.config.ts
@@ -0,0 +1,46 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'green',
+      neutral: 'slate'
+    },
+    footer: {
+      slots: {
+        root: 'border-t border-default',
+        left: 'text-sm text-muted'
+      }
+    }
+  },
+  seo: {
+    siteName: 'Lost Blogs'
+  },
+  header: {
+    title: '',
+    to: '/',
+    logo: {
+      alt: '',
+      light: '',
+      dark: ''
+    },
+    search: true,
+    colorMode: true,
+    links: [{
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/o0olele',
+      'target': '_blank',
+      'aria-label': 'GitHub'
+    }]
+  },
+  footer: {
+    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    colorMode: false,
+    links: [{
+      'icon': 'i-simple-icons-github',
+      'to': 'https://github.com/o0olele',
+      'target': '_blank',
+      'aria-label': 'Nuxt UI on GitHub'
+    }]
+  },
+  toc: {
+  }
+})
